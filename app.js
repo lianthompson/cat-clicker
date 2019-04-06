c6723b82c56752fcc8a3654fc81abec3329a93f6
Lian <thompsonlian@yahoo.com>
2019-04-06 16:16:10 -0700

    remove commented out code

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// let cat1 = document.querySelector('.cat1');
-// let cat2 = document.querySelector('.cat2');
-// let cat3 = document.querySelector('.cat3');
-// let counter1 = document.querySelector('.counter1');
-// let counter2 = document.querySelector('.counter2');
-// let counter3 = document.querySelector('.counter3');
 let cat1Clicks = 0;
 let cat2Clicks = 0;
 let cat3Clicks = 0;
@@ -15,34 +9,33 @@ let catname = document.querySelector('.cat-name');
 let image = document.querySelector('.cat-image');
 let counter = document.querySelector('.counter');
 
-// console.log(cat1);
-// console.log(counter1);
-// console.log(image);
-
 cat1name.innerHTML = "Satoshi";
 cat2name.innerHTML = "Nakamoto";
 cat3name.innerHTML = "Vitalik";
 
 cat1name.addEventListener('click', function(){
+    let clicks = 0;
     catname.innerHTML = "You clicked Satoshi";
     image.src = "./cat.jpg";
     counter.innerHTML = "Satoshi: " + clicks;
 }, false);
 
 cat2name.addEventListener('click', function(){
+    let clicks = 0;
     catname.innerHTML = "You clicked Nakamoto";
     image.src = "./cat1.jpg";
     counter.innerHTML = "Nakamoto: " + clicks;
 }, false);
 
 cat3name.addEventListener('click', function(){
+    let clicks = 0;
     catname.innerHTML = "You clicked Vitalik";
     image.src = "./cat3.jpg";
     counter.innerHTML = "Vitalik: " + clicks;
 }, false);
 
 image.addEventListener('click', function(){
-    counter.innerHTML = "You've clicked " + clicks++ + " times";
+    counter.innerHTML = clicks++;
 })
 
 // cat1.addEventListener('click', function(){
