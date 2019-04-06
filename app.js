41c9ef3ff8c6a6df30bc80262cc65fa4a09ea0af
Lian <thompsonlian@yahoo.com>
2019-04-05 17:59:56 -0700

    add second cat

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-let catButton = document.querySelector('.cat-button');
-let counter = document.querySelector('.counter');
-let clicks = 0;
+let cat1 = document.querySelector('.cat1');
+let cat2 = document.querySelector('.cat2');
+let counter1 = document.querySelector('.counter1');
+let counter2 = document.querySelector('.counter2');
+let cat1Clicks = 0;
+let cat2Clicks = 0;
+let cat1name = document.querySelector('.cat1-name');
+let cat2name = document.querySelector('.cat2-name');
 
-console.log(catButton);
-console.log(counter);
+console.log(cat1);
+console.log(counter1);
 
-catButton.addEventListener('click', function(){
-    clicks++;
-    counter.innerHTML = "Counter: " + clicks;
+cat1name.innerHTML = "Satoshi";
+cat2name.innerHTML = "Nakamoto";
+
+cat1.addEventListener('click', function(){
+    cat1Clicks++;
+    counter1.innerHTML = "Cat 1: " + cat1Clicks;
+}, false);
+
+cat2.addEventListener('click', function(){
+    cat2Clicks++;
+    counter2.innerHTML = "Cat 2: " + cat2Clicks;
 }, false);
